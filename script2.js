f60e7779090bf99c34d7f56f47836e4d6770e7e8
shuvxm <kumarshubham5559@gmail.com>
2024-11-24 14:14:13 +0530

    revise

--- script2.js
+++ script2.js
@@ -1,2 +1,23 @@
-let names = 'simran das'
-console.log(names)
+let names = "simran das";
+console.log(names);
+
+// var es5 js se hai or let const es6 js se hai
+// var funcion scope hota hai, mtlv var jis bhi variable me use hua hai voh variable function ke andar declare hai tho throught function me use krskte
+function sayHello() {
+  for (var i = 1; i < 11; i++) {
+    console.log(i);
+  }
+  console.log("---")
+  console.log(i);
+}
+sayHello()
+
+// let braces scope hota hai
+function sayHello() {
+    for (let i = 1; i < 11; i++) {
+      console.log(i);
+    }
+    // console.log("---")
+    // console.log(i); // error
+  }
+  sayHello()
