30dedac6afa457d51500591c3bdafd6db8c95858
shuvxm <kumarshubham5559@gmail.com>
2024-11-25 21:40:07 +0530

    revise

--- script2.js
+++ script2.js
@@ -1,8 +1,9 @@
 let names = "simran das";
 console.log(names);
 
-// var es5 js se hai or let const es6 js se hai
+// var es5 version se js me hai or let const es6 js se hai
 // var funcion scope hota hai, mtlv var jis bhi variable me use hua hai voh variable function ke andar declare hai tho throught function me use krskte
+// var add itself to the window object
 function sayHello() {
   for (var i = 1; i < 11; i++) {
     console.log(i);
@@ -12,7 +13,8 @@ function sayHello() {
 }
 sayHello()
 
-// let braces scope hota hai
+// let const braces scope hota hai
+// let const does not add itself to the window object
 function sayHello() {
     for (let i = 1; i < 11; i++) {
       console.log(i);
@@ -20,4 +22,4 @@ function sayHello() {
     // console.log("---")
     // console.log(i); // error
   }
-  sayHello()
+sayHello()
